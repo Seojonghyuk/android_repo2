4b2908444d6238a56abb1272d681038960c95461
Seo Jonghyuk <seojonghyuk97@gmail.com>
2023-03-21 15:33:54 +0900

    [create] 코틀린

---
+++ app/src/main/java/com/example/a20230321/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a20230321
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.recyclerkt.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
